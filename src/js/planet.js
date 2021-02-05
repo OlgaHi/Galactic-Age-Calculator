931f1b33c8a22ea320cd8094d1cf4b455aeba0b5
Olha Hizhytska <olgainfotech@gmail.com>
2021-02-05 15:11:11 -0800

    Should calculate and update property planetEarthYears for mars object, test pass

--- src/js/planet.js
+++ src/js/planet.js
@@ -9,11 +9,14 @@ export default class Planet {
   calculatePlanetEarthYears() {
     const mercuryYears = .24;
     const venusYears = .62;
+    const marsYears = 1.88;
     
     if (this.planetName === "Mercury") {
       this.planetEarthYears = Math.round((365 * mercuryYears));
     } else if (this.planetName === "Venus") {
       this.planetEarthYears = Math.round((365 * venusYears));
-    }
+    } else if (this.planetName === "Mars") {
+      this.planetEarthYears = Math.round((365 * marsYears));
+    } 
   }
 }
